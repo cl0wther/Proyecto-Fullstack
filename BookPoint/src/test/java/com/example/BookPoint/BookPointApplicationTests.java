74ba8be33d5d0eeb77a61343ef09727b16fdbaa2
cl0wther <Fa.millar@duocuc.cl>
2026-04-29 09:41:40 -0400

    primer commit: base de proyecto

---
+++ BookPoint/src/test/java/com/example/BookPoint/BookPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
